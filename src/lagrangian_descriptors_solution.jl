dd16563774e0c39bfa7a7c17487cf1e032c6412a
Ricardo M. S. Rosa <rmsrosa@gmail.com>
2022-09-20 14:32:44 -0300

    method for solution

--- src/lagrangian_descriptors_solution.jl
+++ src/lagrangian_descriptors_solution.jl
@@ -3,3 +3,22 @@ struct LagrangianDescriptorsSolution{T1, T2, T3}
     uu0::T2
     direction::T3
 end
+
+function (lagsol::LagrangianDescriptorsSolution)(direction::Symbol=:both)
+    if direction == :both || direction == :total
+        return getindex.(lagsol.enssol.u, :lfwd) + getindex.(lagsol.enssol.u, :lbwd)
+    elseif direction == :forward
+        return getindex.(lagsol.enssol.u, :lfwd)
+    elseif direction == :backward
+        return getindex.(lagsol.ensso.u, :lbwd)
+    elseif direction == :difference
+        return getindex.(lagsol.enssol.u, :lfwd) - getindex.(lagsol.enssol.u, :lbwd)
+    else
+        throw(
+            ArgumentError(
+                "The given argument should be a valid direction:\n" +
+                "`:forward`, `:backward`, `:both` or `:total`, or `:difference`"
+            )
+        )
+    end
+end

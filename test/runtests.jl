72dfa4e099d50c8cf96f9385cb455a160dd96bf5
Ricardo Rosa <rmsrosa@gmail.com>
2022-09-23 23:25:16 -0300

    test plot recipe

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,7 @@
 using LagrangianDescriptors
-using OrdinaryDiffEq, Test
+using OrdinaryDiffEq, Plots, QuadGK, Test
 using LagrangianDescriptors: augmentprob
 using LinearAlgebra: norm
-using QuadGK: quadgk
 using BenchmarkTools: @btime
 
 @testset "Augmented ODEProblems" begin
@@ -259,4 +258,33 @@ end =#
             direction = :blah,
         )
     end
+end
+
+@testset "Plot recipe" begin
+    f! = function (du, u, p, t)
+        x, y = u
+        A, ω = p
+        du[1] = y
+        du[2] = x - x^3 + A * cos(ω * t)
+    end
+    
+    u0 = [0.5, 2.2]
+    tspan = (0.0, 13.0)
+    A = 0.3; ω = π; p = (A, ω)
+    
+    prob = ODEProblem(f!, u0, tspan, p)
+
+    M = function (du, u, p, t) norm(du) end
+
+    uu0 = [[x, y] for y in range(-1.0, 1.0, length=301), x in range(-1.8, 1.8, length=301)]
+
+    lagprob = @test_nowarn LagrangianDescriptorProblem(prob, M, uu0)
+
+    lagsol = @test_nowarn solve(lagprob, Tsit5())
+
+    @test_nowarn plot(lagsol)
+    @test_nowarn plot(lagsol, :both)
+    @test_nowarn plot(lagsol, :forward)
+    @test_nowarn plot(lagsol, :backward)
+    @test_throws ArgumentError plot(lagsol, :blah)
 end

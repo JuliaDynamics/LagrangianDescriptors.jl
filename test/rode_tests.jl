1adfa72be68041be7750bb286c04fe9bb5e8876a
Ricardo Rosa <rmsrosa@gmail.com>
2022-09-26 20:33:12 -0300

    more rode tests

--- test/rode_tests.jl
+++ test/rode_tests.jl
@@ -24,53 +24,83 @@ using LinearAlgebra: norm
         augsol = @test_nowarn solve(augprob, RandomHeun(), dt=1/100)
         @test length(augsol.u[begin]) == 4
         @test augsol.u[begin].fwd == augsol.u[begin].bwd == u0
-        #@test augsol.u[end].fwd ≈ u0 * exp(p * tf)
-        #@test augsol.u[end].bwd ≈ u0 * exp(-p * tf)
+        @test augsol.t ≈ augsol.W.t
+        @test augsol.u[end].lfwd ≈ sum(M.(f.(getindex.(augsol.u, :fwd), p, augsol.t, augsol.W.u), getindex.(augsol.u, :fwd), p, augsol.t, augsol.W.u)[begin + 1:end] .* (augsol.t[begin + 1:end] - augsol.t[begin:end-1])) rtol = 0.01
+        @test augsol.u[end].lbwd ≈ sum(M.(f.(getindex.(augsol.u, :bwd), p, augsol.t, augsol.W.u), getindex.(augsol.u, :bwd), p, augsol.t, augsol.W.u)[begin + 1:end] .* (augsol.t[begin + 1:end] - augsol.t[begin:end-1])) rtol = 0.01
         @test augsol.u[begin].lfwd ≈ 0.0
-        #@test augsol.u[end].lfwd ≈ p * u0^2 * (exp(2 * p * tf) - 1) / 2 atol = 0.01
         @test augsol.u[begin].lbwd ≈ 0.0
-        #@test augsol.u[end].lbwd ≈ p * u0^2 * (1 - exp(-2 * p * tf)) / 2 atol = 0.01
 
         augprob = @test_nowarn augmentprob(prob, M; direction = :forward)
         augsol = @test_nowarn solve(augprob, RandomHeun(), dt=1/100)
         @test length(augsol.u[begin]) == 2
         @test augsol.u[begin].fwd == u0
-        #@test augsol.u[end].fwd ≈ u0 * exp(p * tf)
         @test augsol.u[begin].lfwd ≈ 0.0
-        #@test augsol.u[end].lfwd ≈ p * u0^2 * (exp(2 * p * tf) - 1) / 2 atol = 0.01
+        @test augsol.u[end].lfwd ≈ sum(M.(f.(getindex.(augsol.u, :fwd), p, augsol.t, augsol.W.u), getindex.(augsol.u, :fwd), p, augsol.t, augsol.W.u)[begin + 1:end] .* (augsol.t[begin + 1:end] - augsol.t[begin:end-1])) rtol = 0.01
 
         augprob = @test_nowarn augmentprob(prob, M; direction = :backward)
         augsol = @test_nowarn solve(augprob, RandomHeun(), dt=1/100)
         @test length(augsol.u[begin]) == 2
         @test augsol.u[begin].bwd == u0
-        #@test augsol.u[end].bwd ≈ u0 * exp(-p * tf)
         @test augsol.u[begin].lbwd ≈ 0.0
-        #@test augsol.u[end].lbwd ≈ p * u0^2 * (1 - exp(-2 * p * tf)) / 2 atol = 0.01
+        @test augsol.u[end].lbwd ≈ sum(M.(f.(getindex.(augsol.u, :bwd), p, augsol.t, augsol.W.u), getindex.(augsol.u, :bwd), p, augsol.t, augsol.W.u)[begin + 1:end] .* (augsol.t[begin + 1:end] - augsol.t[begin:end-1])) rtol = 0.01
 
         @test_throws ArgumentError augmentprob(prob, M; direction = :blah)
     end
 
+    @testset "Cubic IIP" begin
+        f = function (u)
+            u .- u .^ 3
+        end
+        f! = function (du, u, p, t, W)
+            du .= f.(u) .+ p * W
+        end
+        t0 = 0.0
+        tf = 5.0
+        tspan = (t0, tf)
+        p = 0.01
+        n = 5
+        u0 = 0.1 .+ 0.7rand(n)
+        prob = RODEProblem(f!, u0, tspan, p, rand_prototype=zeros(n))
+        tspanbwd = (tf, t0)
+        probbwd = remake(prob, tspan = (tf, t0))
+        solfwd = @test_nowarn solve(prob, RandomHeun(), dt=1/100)
+        solbwd = @test_nowarn solve(probbwd, RandomHeun(), dt=1/100)
+
+        M = function (du, u, p, t, W)
+            norm(du)
+        end
+        augprob = @test_nowarn augmentprob(prob, M)
+
+        augsol = @test_nowarn solve(augprob, RandomHeun(), dt=1/100, save_noise=true)
+        @test length(augsol.u[begin]) == 2n + 2
+        @test augsol.u[begin].fwd == augsol.u[begin].bwd == u0
+        @test augsol.u[begin].lfwd == augsol.u[begin].lbwd == 0.0
+        @test all(≥(0.9), augsol.u[end].fwd)
+        @test all(≤(0.1), augsol.u[end].bwd)
+        @test augsol.u[end].lfwd ≈ sum(M.(f.(getindex.(augsol.u, :fwd)) .+ p .* augsol.W.u, getindex.(augsol.u, :fwd), p, augsol.t, augsol.W.u)[begin + 1:end] .* (augsol.t[begin + 1:end] - augsol.t[begin:end-1])) rtol = 0.01
+        @test augsol.u[end].lbwd ≈ sum(M.(f.(getindex.(augsol.u, :bwd)) .+ p .* augsol.W.u, getindex.(augsol.u, :bwd), p, augsol.t, augsol.W.u)[begin + 1:end] .* (augsol.t[begin + 1:end] - augsol.t[begin:end-1])) rtol = 0.01
+    end
 end
 
 @testset "Lagrang Descr" begin
-    @testset "linear OOP ODEProblem" begin
-        f = function (u, p, t)
-            u
+    @testset "linear OOP RODEProblem" begin
+        f = function (u, p, t, W)
+            u + p * W
         end
         p = 0.1
         t0 = 0.0
         tf = 5.0
         tspan = (t0, tf)
         u0 = 0.5
-        prob = ODEProblem(f, u0, tspan)
+        prob = RODEProblem(f, u0, tspan, p)
 
-        M = function (du, u, p, t)
+        M = function (du, u, p, t, W)
             sum(abs2, du)
         end
         uu0 = range(-1.0, 1.0, length = 101)
         lagprob = @test_nowarn LagrangianDescriptorProblem(prob, M, uu0)
-        lagsol = @test_nowarn solve(lagprob, Tsit5())
-        @test_nowarn solve(lagprob, Tsit5(), EnsembleSerial())
+        lagsol = @test_nowarn solve(lagprob, RandomHeun(), dt=1/100)
+        @test_nowarn solve(lagprob, RandomHeun(), dt=1/100, EnsembleSerial())
 
         @test lagsol() == lagsol(:total) == lagsol(:both) ≈ sum.(lagsol.enssol.u)
         @test lagsol(:forward) ≈ getindex.(lagsol.enssol.u, :lfwd)
@@ -81,16 +111,16 @@ end
 
         lagprob =
             @test_nowarn LagrangianDescriptorProblem(prob, M, uu0; direction = :forward)
-        lagsol = @test_nowarn solve(lagprob, Tsit5())
-        @test_nowarn solve(lagprob, Tsit5(), EnsembleSerial())
+        lagsol = @test_nowarn solve(lagprob, RandomHeun(), dt=1/100)
+        @test_nowarn solve(lagprob, RandomHeun(), dt=1/100, EnsembleSerial())
 
         @test lagsol() ≈ sum.(lagsol.enssol.u) ≈ lagsol(:forward)
         @test lagsol(:forward) ≈ getindex.(lagsol.enssol.u, :lfwd)
 
         lagprob =
             @test_nowarn LagrangianDescriptorProblem(prob, M, uu0; direction = :backward)
-        lagsol = @test_nowarn solve(lagprob, Tsit5())
-        @test_nowarn solve(lagprob, Tsit5(), EnsembleSerial())
+        lagsol = @test_nowarn solve(lagprob, RandomHeun(), dt=1/100)
+        @test_nowarn solve(lagprob, RandomHeun(), dt=1/100, EnsembleSerial())
 
         @test lagsol() ≈ sum.(lagsol.enssol.u) ≈ lagsol(:backward)
         @test lagsol(:backward) ≈ getindex.(lagsol.enssol.u, :lbwd)

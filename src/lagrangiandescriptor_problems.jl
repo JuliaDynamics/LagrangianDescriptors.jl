23421d90ea31eec20460314fa583222c6ab9027c
Ricardo Rosa <rmsrosa@gmail.com>
2022-09-29 21:36:11 -0300

    clean up inner constructor

--- src/lagrangiandescriptor_problems.jl
+++ src/lagrangiandescriptor_problems.jl
@@ -63,45 +63,51 @@ struct LagrangianDescriptorProblem{T1,T2,T3}
     direction::T3
 
     function LagrangianDescriptorProblem(prob, M, uu0; direction::Symbol = :both)
-        if direction == :both
-            prob_func = function (augprob, i, repeat; uu0 = uu0)
-                remake(
-                    augprob,
-                    u0 = ComponentVector(
-                        fwd = uu0[i],
-                        bwd = uu0[i],
-                        lfwd = 0.0,
-                        lbwd = 0.0,
-                    ),
-                )
-            end
-            output_func = function (sol, i)
-                (ComponentArray(lfwd = last(sol).lfwd, lbwd = last(sol).lbwd), false)
-            end
-        elseif direction == :forward
-            prob_func = function (augprob, i, repeat; uu0 = uu0)
-                remake(augprob, u0 = ComponentVector(fwd = uu0[i], lfwd = 0.0))
-            end
-            output_func = function (sol, i)
-                (ComponentArray(lfwd = last(sol).lfwd), false)
-            end
-        elseif direction == :backward
-            prob_func = function (augprob, i, repeat; uu0 = uu0)
-                remake(augprob, u0 = ComponentVector(bwd = uu0[i], lbwd = 0.0))
-            end
-            output_func = function (sol, i)
-                (ComponentArray(lbwd = last(sol).lbwd), false)
-            end
-        else
-            throw(
-                ArgumentError(
-                    "Keyword argument `direction = $direction` not implemented; use either `:forward`, `:backward` or `:both`",
+        ensprob = get_ensemble_problem(prob, M, uu0; direction)
+        return new{typeof(ensprob),typeof(uu0),typeof(direction)}(ensprob, uu0, direction)
+    end    
+end
+
+function get_ensemble_problem(prob, M, uu0; direction::Symbol=both)
+    if direction == :both
+        prob_func = function (augprob, i, repeat; uu0 = uu0)
+            remake(
+                augprob,
+                u0 = ComponentVector(
+                    fwd = uu0[i],
+                    bwd = uu0[i],
+                    lfwd = 0.0,
+                    lbwd = 0.0,
                 ),
             )
         end
-
-        augprob = augmentprob(prob, M; direction)
-        ensprob = EnsembleProblem(augprob, prob_func = prob_func, output_func = output_func)
-        new{typeof(ensprob),typeof(uu0),typeof(direction)}(ensprob, uu0, direction)
+        output_func = function (sol, i)
+            (ComponentArray(lfwd = last(sol).lfwd, lbwd = last(sol).lbwd), false)
+        end
+    elseif direction == :forward
+        prob_func = function (augprob, i, repeat; uu0 = uu0)
+            remake(augprob, u0 = ComponentVector(fwd = uu0[i], lfwd = 0.0))
+        end
+        output_func = function (sol, i)
+            (ComponentArray(lfwd = last(sol).lfwd), false)
+        end
+    elseif direction == :backward
+        prob_func = function (augprob, i, repeat; uu0 = uu0)
+            remake(augprob, u0 = ComponentVector(bwd = uu0[i], lbwd = 0.0))
+        end
+        output_func = function (sol, i)
+            (ComponentArray(lbwd = last(sol).lbwd), false)
+        end
+    else
+        throw(
+            ArgumentError(
+                "Keyword argument `direction = $direction` not implemented; use either `:forward`, `:backward` or `:both`",
+            ),
+        )
     end
+
+    augprob = augmentprob(prob, M; direction)
+    ensprob = EnsembleProblem(augprob, prob_func = prob_func, output_func = output_func)
+
+   return ensprob
 end
